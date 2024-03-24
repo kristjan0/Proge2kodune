e39222d1d9fe0aac2170d48d3828be03d0884518
getr <123596128+Getuh@users.noreply.github.com>
2024-03-24 20:54:49 +0200

    Delete getr_ylesanne2 directory

--- getr_ylesanne2/yl2.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int: #pmst võtab kaks asja haystack ja needle
-        
-        if needle in haystack: #otsid needlet haystackist
-            return haystack.index(needle) #returnib kui leiab sõna teisest sõnast
-        else:
-            return -1 #kui ei leia siis annab -1
