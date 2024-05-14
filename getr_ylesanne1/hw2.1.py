817da7a459636fd203c5934c09de29039dce015c
kristjan0 <1533424+kristjan0@users.noreply.github.com>
2024-05-14 21:25:37 +0300

    Getter ei oska

--- getr_ylesanne1/hw2.1.py
+++
@@ -1,8 +0,0 @@
-  def addDigits(self, num: int) -> int:
-        if num<10: #kui arv on vaiksem, kui 10 returnib selle
-            return num
-
-        if num%9==0:
-            return 9
-        else:
-            return num%9 #kui miski muu siis returnib numbri jaagi
--- getr_ylesanne2/hw2.2.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        i = 0 #panen kirja muutuja algab 0
-        for x in nums: #loopiga vaatab koik yle
-            i^=x #xor ehk kui on samad siis result 0 ja kui erinev result 1 vmidagi
-        return i #tagastab numbri mis ainuke listis
