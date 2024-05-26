55fac45c1c7a2e7841860c094065c5d0bfbe605b
Getuh <123596128+Getuh@users.noreply.github.com>
2024-05-26 14:37:24 +0300

    did yl 6.1

---
+++ getr_ylesanne6.1/yl6.1.py
@@ -0,0 +1,11 @@
+#219. Contains Duplicate II
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        numsMap = {}
+        for i in range(len(nums)):
+            if nums[i] in numsMap and i - numsMap[nums[i]] <= k: 
+                    return True
+            else:
+                numsMap[nums[i]] = i
+        return False
